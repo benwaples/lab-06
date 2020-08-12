28a8dc0251c963ee0250295a74db01c7777f5cfd
bwapes <benwaples@gmail.com>
2020-08-11 17:08:15 -0700

    added the trails function

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const request = require('superagent');
 const cors = require('cors');
-const weatherData = require('./data/weather.js');const app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 const {
-    GEOCODE_API_KEY
+    GEOCODE_API_KEY,
+    WEATHERBIT_KEY,
+    HIKING_KEY
 } = process.env;
 
 app.use(cors());
@@ -18,7 +20,6 @@ async function getLatLong(cityName) {
 
     const city = response.body[0];
 
-    
     return {
         formatted_query: city.display_name,
         latitude: city.lat,
@@ -38,27 +39,66 @@ app.get('/location', async(req, res) => {
 });
 
 async function getWeather(lat, lon) {
-    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}3&key=${WEATHERBIT_KEY}`);
 
-    const city = response.body[0];
-
-    const forecastArray = city.map(weatherItem => {
+    const city = response.body.data;
+    const forecast = city.map(weather => {
         return {
-            forecast: weatherItem.weather.description,
-            date: new Date(weatherItem.ts * 1000)
+            forecast: weather.weather.description,
+            time: new Date(weather.ts * 1000)
         };
     });
-    
-    return forecastArray;
+
+    return forecast.slice(0, 3);
 }
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async(req, res) => {
     try {
         const userLat = req.query.latitude;
         const userLon = req.query.longitude;
 
-        const mungedData = getWeather(userLat, userLon);
+        const mungedData = await getWeather(userLat, userLon);
+
         res.json(mungedData);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+    
+});
+
+async function getHike(lat, lon) {
+    const response = await request.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=200&key=${HIKING_KEY}`);
+
+    const hikes = response.body.trails;
+    
+    const allHikes = hikes.map(hike => {
+        return {
+            trail_url: hike.trail_url,
+            name: hike.name,
+            location: hike.location,
+            length: hike.length,
+            condition_date: new Date(hike.conditionDate).toDateString(),
+            condition_time: new Date(hike.conditionDate).toTimeString(),
+            conditions: hike.conditionStatus,
+            stars: hike.stars,
+            star_votes: hike.starVotes,
+            summary: hike.summary
+        };
+    });
+    
+    return allHikes;
+}
+
+app.get('/trails', async(req, res) => {
+    try {
+        const userLat = req.query.latitude;
+        const userLon = req.query.longitude;
+
+        const mungedData = await getHike(userLat, userLon);
+        console.log(mungedData, 'city repsonse');
+        res.json(mungedData);
+        
+
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
